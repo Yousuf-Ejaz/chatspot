864a3c57d65cc4df4fb03550a56cdd44d0ec8e51
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2023-02-26 17:17:30 +0530

    feat: added removeuser adduser and renamegroup features on the backend

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,14 +4,17 @@ const {
 	accessChat,
 	fetchChats,
 	createGroupChat,
+	renameGroup,
+	addToGroup,
+	removeFromGroup,
 } = require("../controllers/chatControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat).get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("groupadd").put(protect, addToGroup);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
