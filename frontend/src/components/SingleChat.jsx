5f22a8e0d578341c3efa92bcbdcf47fd4ab07a8a
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2023-03-04 02:49:30 +0530

    fix: changed the endpoint to the hosted backend's endpoint

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -15,7 +15,8 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../context/ChatProvider";
-const ENDPOINT = "http://localhost:5000";
+// const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chatspot-api.onrender.com/";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
